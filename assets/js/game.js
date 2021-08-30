e86582e43ee58d4944343cf695e88b552cc5794f
AS <asadrgilany7@gmail.com>
2021-08-30 14:32:43 -0400

    added enemy array and had the fight function cycle through each enemy

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,21 +1,21 @@
+// Game States
+// "WIN" - Player robot has defeated all enemy robots
+//      * Fight all enemy robots
+//      * Defeat each enemy robot
+// "LOSE" - Player robot's health is zero or less
+
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
-// check to see if the value of the playerHealth variable is greater than 0
-if (playerHealth > 0) {
-    console.log("Your player is still alive.");
-}
 var playerAttack = 10;
 var playerMoney = 10;
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-console.log(playerName, playerAttack, playerHealth);
-
 // this creates a function named "fight"
 
-var fight = function() {
+var fight = function(enemyName) {
     
     // alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
@@ -83,4 +83,6 @@ var fight = function() {
     }
 };
 
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
